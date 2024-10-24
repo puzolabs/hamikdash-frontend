d474ebe3e875dc42e2df810b8001f1db625a2c09
Meir <puzolabs@gmail.com>
2024-10-24 20:27:50 +0300

    read section property

--- hamikdash_sheli/lib/dataTypes/korban_case.dart
+++ hamikdash_sheli/lib/dataTypes/korban_case.dart
@@ -2,19 +2,33 @@
 import 'package:hamikdash_sheli/dataTypes/korban.dart';
 import 'package:hamikdash_sheli/dataTypes/korbans_option.dart';
 
+// 7 sections
+enum Sections {
+  none,
+  happyEvent,
+  nedarimUndavot,
+  nazir, // in the middle since one event is happy and the other one is part khet part avon
+  caparatHataim,
+  caparatAvonot,
+  mezora, // the overall context of mezora is bad and sad so its in the last but least since he was able to recover and become tahor
+  sadEvent,
+}
+
 class KorbanCase
 {
+  Sections section;
   String title;
   List<Korban>? korbanot;
   List<KorbansOption>? korbanotOptions;
 
-  KorbanCase({this.title = "", this.korbanot, this.korbanotOptions });
+  KorbanCase({this.section = Sections.none, this.title = "", this.korbanot, this.korbanotOptions });
 
   bool get isSingleOptionKorbanCase => korbanot != null && korbanotOptions == null;
   bool get isMultiOptionsKorbanCase => korbanot == null && korbanotOptions != null;
   bool get isComplexKorbanCase => korbanot != null && korbanotOptions != null ;
 
   factory KorbanCase.fromJson(Map<String, dynamic> map) {
+    String section = map["section"];
     String title = map["title"];
     var korbanot = map["korbanot"];
     var korbanotOptions = map["korbanotOptions"];
@@ -27,6 +41,6 @@ class KorbanCase
       ?.map<KorbansOption>((option) => KorbansOption.fromJson(option))
       .toList();
 
-    return KorbanCase(title: title, korbanot: list, korbanotOptions: optionsList);
+    return KorbanCase(section: Sections.values.byName(section), title: title, korbanot: list, korbanotOptions: optionsList);
   }
 }
