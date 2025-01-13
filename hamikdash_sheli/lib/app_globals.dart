e83a8b753e7e45f8c90e006377b4035ce068620b
Meir <puzolabs@gmail.com>
2025-01-13 08:36:57 +0200

    prepare for using timezone

---
+++ hamikdash_sheli/lib/app_globals.dart
@@ -0,0 +1,7 @@
+import 'package:timezone/timezone.dart';
+
+class AppGlobals {
+  Location? jerusalem;
+}
+
+AppGlobals app_globals = AppGlobals();
--- hamikdash_sheli/lib/main.dart
+++ hamikdash_sheli/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hamikdash_sheli/app_globals.dart';
 import 'package:hamikdash_sheli/app_persistence.dart';
 import 'package:hamikdash_sheli/app_state.dart';
 import 'package:hamikdash_sheli/cal/cal_loader.dart';
@@ -6,6 +7,7 @@ import 'package:hamikdash_sheli/dataTypes/user.dart';
 import 'package:hamikdash_sheli/korbanot_config_files_loader.dart';
 import 'package:hamikdash_sheli/pages/welcome_page.dart';
 
+import 'package:timezone/timezone.dart' as tz;
 import 'package:timezone/data/latest_10y.dart' as tz;
 
 void main() async {
@@ -16,6 +18,7 @@ void main() async {
   await appPersistence.load();
 
   tz.initializeTimeZones();
+  app_globals.jerusalem = tz.getLocation('Asia/Jerusalem');
   
   appState.user = User(name: "meir", email:"a@b.com");
 
